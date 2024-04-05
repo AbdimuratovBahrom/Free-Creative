93ce9c43a09b2433e2b361d7853a3d0a8ee83eba
Your Name <bahrom9791@gmail.com git config --global user.name>
2024-04-06 00:46:08 +0500

    fix5 bug

---
+++ js/sent.js
@@ -0,0 +1,31 @@
+
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+ 
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('telphone').value;
+  var email = document.getElementById('checkbox').value;
+  // var message = document.getElementById('message').value;
+ 
+  // Здесь будет код для отправки данных на сервер
+ 
+  alert('Сообщение отправлено!');
+  this.reset();
+});
+
+
+
+
+document.getElementById('feedbackForm3').addEventListener('submit', function(event) {
+  event.preventDefault();
+ 
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('telphone').value;
+  var email = document.getElementById('checkbox').value;
+  // var message = document.getElementById('message').value;
+ 
+  // Здесь будет код для отправки данных на сервер
+ 
+  alert('Сообщение отправлено!');
+  this.reset();
+});
